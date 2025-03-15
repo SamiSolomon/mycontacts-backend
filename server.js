d4fc9ddd3106276e1fed8265833b309f511c4526
Samuel Solomon <samuelsolomon452@gmail.com>
2025-03-15 16:51:19 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(express.json())
+
+app.use("/api/contacts", require("./routes/contactRoutes"))
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`)
+})
+
+
+
