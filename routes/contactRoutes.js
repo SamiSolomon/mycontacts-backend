abd2a2388d812266b2b86b03bbdbd4a082888b50
Samuel Solomon <samuelsolomon452@gmail.com>
2025-03-19 17:56:12 +0300

    creating database for contact

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { getContacts, createContact, getContact, updateContact, deleteContact } =
-  require("../controller/contactController").default;
+  require("../controller/contactController");
 
 router.route("/").get(getContacts).post(createContact);
 router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
 connectDb();
